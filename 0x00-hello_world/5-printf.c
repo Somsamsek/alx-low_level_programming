c0956b9042f3865228bd71384980cce71c455de1
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-06-30 21:54:38 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*
- *  *main - Entry point
- *   *Return: Always 0 (Success)
+/**
+ *  main - Entry point
+ *   Return: Always 0 (Success)
  */
 
-/*Function main begins to execute program*/
 int main(void)
 {
-	/*maindescription*/
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+			return (0);
 }
