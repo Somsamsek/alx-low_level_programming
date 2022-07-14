202a21476e9e7c782cf82d5bd399c80227e20d5f
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-13 21:30:21 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -11,15 +11,20 @@ int main(void)
 {
 	int a, b;
 	int *pa, *pb;
-	
+
 	a = 98;
 	b = 42;
-	
+
+	printf("These are the original values. a is: %d & b is: %d\n", a, b);
+
 	pa = &a;
 	pb = &b;
 
+	printf("These are the addresses. pa is: %p & pb is: %p\n", pa, pb);
+
 	*pa = 42;
 	*pb = 98;
+	printf("These are the swapped values. a is: %d & b is: %d\n.", a, b);
 
 	return (0);
 }
