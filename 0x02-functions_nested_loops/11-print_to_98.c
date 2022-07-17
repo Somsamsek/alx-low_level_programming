12fa28576ccd00935b8d1fd51bd48bb07a7a44bb
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-17 14:44:50 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,14 +3,13 @@
 
 /**
  * print_to_98 - a function that prints from n to 98.
- * 
  * @n: integer argument.
  * Return: void.
  */
 
 void print_to_98(int n)
 {
-	while ( n > 98)
+	while (n > 98)
 	{
 		printf("%d, ", n);
 		n--;
