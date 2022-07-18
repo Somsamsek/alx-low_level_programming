ebc776f62ba04c95f325fb64dbd97a9f17e1c05a
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-18 10:47:56 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,26 +7,42 @@
 
 int main(void)
 {
-	int i;
+	int counter, overflow;
 	unsigned long a = 1;
-	unsigned long b = 2;
-	unsigned long fib;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
 
-	printf("%lu, %lu, ", a, b);
+	printf("1");
 
-	for (i = 1; i <= 96; i++)
+	for (counter = 2; counter < 93; counter++)
 	{
-		fib = a + b;
-
-		if (i == 96)
-			printf("%lu", fib);
-		else
-			printf("%lu, ", fib);
+		sum = a + b;
 		a = b;
-		b = fib;
+		b = sum;
+		printf(", %lu", sum);
 	}
 
-	printf("\n\n");
+	a_head = a / 1000000000;
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+
+	for (; counter < 99; counter++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
+	}
+
+	printf("\n");
 
 	return (0);
 }
