2058ed2c85722b43c58a258990612e1baabad657
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-22 06:32:44 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks for a digit (0 - 9)
+ * @c:intgere argument to be checked
+ * Return: 1 if digit, c otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c <= 9))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
