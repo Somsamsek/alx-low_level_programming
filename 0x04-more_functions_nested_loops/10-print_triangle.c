ec9f90fca7b70db16d3e10980697d5c6a7381136
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-23 18:33:53 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - a function that prints a traingle using #
+ * @size: number of # characters
+ * Return: triangle
+ */
+
+void print_triangle(int size)
+{
+	int space, row, count;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (space = row; space < size; space++)
+				_putchar(' ');
+
+			count = 0;
+
+			do {
+				_putchar('#');
+
+				count++;
+			} while (count < row)
+		}
+	}
+}
