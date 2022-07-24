f4c4382d82ff7cf94aac1edd9ecec151163daeea
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-24 16:43:33 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,14 +3,13 @@
 
 /**
  * reset_to_98 - pointer function to update the value of variable.
- *
+ * @*n: pointer
  * Return: Always 0.
  */
 
-void reset_to_98(void)
+void reset_to_98(int *n)
 {
 	int pn;
-	int *n;
 
 	pn = 402;
 	n = &pn;
