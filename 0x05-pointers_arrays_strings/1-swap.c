316d488980ac217d9b730a236af4fe3347522eb3
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-26 13:50:21 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,29 +2,17 @@
 #include "main.h"
 
 /**
- * main - functions swaps the values of int a & b.
- *
+ * swap_int - functions swaps the values of int a & b.
+ * @a: integer argument a
+ * @b: integer argument b
  * Return: Always 0.
  */
 
-int main(void)
+void swap_int(int *a, int *b)
 {
-	int a, b;
-	int *pa, *pb;
+	int n;
 
-	a = 98;
-	b = 42;
-
-	printf("These are the original values. a is: %d & b is: %d\n", a, b);
-
-	pa = &a;
-	pb = &b;
-
-	printf("These are the addresses. pa is: %p & pb is: %p\n", pa, pb);
-
-	*pa = 42;
-	*pb = 98;
-	printf("These are the swapped values. a is: %d & b is: %d\n.", a, b);
-
-	return (0);
+	n = *a;
+	*a = *b;
+	*b = n;
 }
