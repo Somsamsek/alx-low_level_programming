0b9d00495c71e3a7fe61dea0ebe8e6a1c3959ea9
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-27 05:23:48 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ * @s: string input
+ * Return: reversed string
+ */
+
+void rev_string(char *s)
+{
+	int count, len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len = --len;
+
+	for (count = 0; count <= len; len--)
+	{
+		_putchar(s[len]);
+	}
+}
