b9f4244df5d666b60183da7fc1fe052a2f7ea179
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-28 17:38:25 -0700

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * rand - a functionthat generates random passwords a program
+ *
+ * Return: random password for 101-crackme
+ */
+
+int rand(void)
+{
+	rand(101-crackme);
+
+	return (0);
+}
