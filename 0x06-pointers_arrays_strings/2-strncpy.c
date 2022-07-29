8f40ffe2e85a70656e276be345a6e7b08284b46b
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-29 09:58:01 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,11 +19,11 @@ char *_strncpy(char *dest, char *src, int n)
 		{
 			dest[i] = src[i];
 		}
+	}
 
-		while (i < n)
-		{
-			dest[i++] = '\0';
-		}
+	while (i < n)
+	{
+		dest[i++] = '\0';
 	}
 
 	return (dest);
