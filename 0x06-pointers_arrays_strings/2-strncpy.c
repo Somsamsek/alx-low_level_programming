3cf577531f81494bd82a696aa1a6e29eb161a4ae
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-07-29 09:47:45 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string
+ * @dest: copy string  to
+ * @src: copy string from
+ * @n: max number of bytes/characters
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		if (i < n)
+		{
+			dest[i] = src[i];
+		}
+
+		while (i < n)
+		{
+			dest[i++] = '\0';
+		}
+	}
+
+	return (dest);
+}
