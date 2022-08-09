67f8918fd2950b9dcd397d7934029dd18a3c2db7
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-09 16:56:59 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - a function that adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector array
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc <= 1)
+		return (0);
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
