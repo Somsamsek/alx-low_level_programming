f288184de2082b82c50df49292c034607ff1b795
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-08 20:33:38 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a function that prints the number of arguments
+ * @argc: number of argument passed
+ * @argv: array holding strings passed
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
