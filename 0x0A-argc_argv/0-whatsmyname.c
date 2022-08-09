37bb717104aeffe2128b40006b4c0c5598fec869
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-08 20:24:12 -0700

    First commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a function that prints its name
+ * @argc: argument count variable
+ * @argv: argument vector array
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char* argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is the 12th project in the C programming language track. It focuses on the arguments of the the 'main' function in the C standard library.
