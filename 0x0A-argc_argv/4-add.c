47c0fc3a4e4f6fd683d253bb9b120a3b6f45f739
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-09 18:26:16 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "main.h"
 #include <stdlib.h>
 
 /**
+ * is_num - iterate through each argv to test if it's a number
+ * @argvv: a argv
+ * Return: true only if entire string is a number, false if not
+ */
+
+bool is_num(char *argvv)
+{
+	int j = 0;
+
+	for (j = 0; argvv[j]; j++)
+	{
+		if (!(argvv[j] >= '0' && argvv[j] <= '9'))
+			return (0);
+	}
+
+	return (1);
+}
+
+/**
  * main - a function that adds positive numbers
- * @argc: argument count
+ * @argc: argument counter
  * @argv: argument vector array
  * Return: 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i = 1, sum = 0;
 
-	if (argc < 1)
-		return (0);
-
-	for (i = 1; i < argc; i++)
+	if (argc == 1)
 	{
-		if (!atoi(*argv[i]))
+		printf("0\n");
+		return (0);
+	}
+
+	while (i < argc)
+	{
+		if (is_num(argv[i]))
+			sum += atoi(argv[i]);
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-
-		sum += atoi(argv[i]);
+		i++;
 	}
 	printf("%d\n", sum);
 
