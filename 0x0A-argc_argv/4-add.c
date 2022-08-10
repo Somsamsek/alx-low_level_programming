030ca75969cf36ff5bad9e9ff07439dc92e32572
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-09 17:01:51 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,22 +13,20 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc <= 1)
+	if (argc < 1)
 		return (0);
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			if (!atoi(argv[i]))
-			{
-				printf("Error\n");
-				return (1);
-			}
 
-			sum += atoi(argv[i]);
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", sum);
+
+		sum += atoi(argv[i]);
 	}
+	printf("%d\n", sum);
 
 	return (0);
 }
