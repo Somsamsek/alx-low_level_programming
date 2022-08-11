2ac081c8d4e80dfffabd972aac93725ac76a6390
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-11 09:49:28 -0700

    Halfway commi

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - getends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int end1, end2, i = 0;
+	char *array;
+
+	if (s1 == NULL || s2 == NULL)
+		s1 = s2 = "";
+
+	for (end1 = 0; end1 <= *s1; end1++)
+	{
+	}
+
+	for (end2 = 0; end2 <= *s2; end2++)
+	{
+	}
+
+	array = malloc(sizeof(char) * (end1 + end2 + 1));
+
+	if (array == NULL)
+		return (NULL);
+
+	while (*s1)
+	{
+		array[i] = *s1;
+		i++;
+		s1++;
+	}
+
+	while (*s2)
+	{
+		array[i] = *s2;
+		i++;
+		s2++;
+	}
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+This is the 13th project in the C programming learnig track.
+It focuses on the malloc and free memomry manipulation concepts.
