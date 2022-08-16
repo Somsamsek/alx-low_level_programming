e49f40fd85280517db2d5e89ffe887ab1785df48
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-16 13:08:28 -0700

    First half commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	if (d->age == 0.0)
-		printf("(nil)\n");
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("(nil)\n");
