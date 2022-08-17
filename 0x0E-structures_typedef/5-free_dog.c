9e7757a5be360d14f1740119b54386a2822d30d6
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-16 19:08:29 -0700

    Second half commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,6 +10,15 @@
 
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
+
+	if (d->name)
+		free(d->name);
+
+	if (d->owner)
+		free(d->owner);
+
 	free(d);
 
 	return;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define DOG_H
 
 /**
+ * dog_t - new type for struct dog;
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - hold dog information
  * @name:  name of dog
  * @age: dog age
@@ -17,11 +22,6 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-/**
- * dog_t - new type for struct dog
- */
-
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
