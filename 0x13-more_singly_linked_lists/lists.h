70edcd50a6e53b2c2ebd9cdd7ef9550bb5ed0d66
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-16 21:39:34 -0700

    Second commit All

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked listnode structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
