16d7ff9f6fd2d05b01095237704de96e4a03617c
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-17 08:28:14 -0700

    Task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include"lists.h"
+#include <stdlib.h>
+
+/**
+ * reverse_listint - reverselinked list
+ * @head: pointer to head pointer of linked list
+ * Return: pointer to first node of reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *next;
+
+	/* account for no ptr and empty list */
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	/* iterate thorough list to reverse linking */
+	prev = NULL;
+	while (*head != NULL)
+	{
+		next = (*head)->next; /* keep track of next node to move to */
+		(*head)->next = prev; /* link current node's ptr to prev node */
+		prev = *head; /* update previous node to be current node */
+		*head = next; /* ove current node to next node */
+	}
+	(*head) = prev; /* unlink last element from null amd point backwards */
+
+	return (*head);
+}
