f0b385efc7d4178bc04334197366b1a4b0e997ad
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-18 21:55:28 -0700

    first commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - get bit at index
+ * @n: number
+ * @index: index within binary number
+ * Return: bit 0 or 1, or -1 if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+	unsigned int max_bits;
+
+	/* validate index is not out of range */
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
+		return (-1);
+
+	/* shift number index place right to find bit */
+	bit = ((n >> index) & 1);
+
+	return (bit);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This is the 26th project in the C programming language learning track. It focuses on the concept of bit wise manipulation.
