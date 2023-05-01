1cb67eeab771b1bf14f982de6fb82da415ac5f71
Somsamsek <170404060@live.unilag.edu.ng>
2023-05-01 16:46:54 -0700

    Last grind

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-#endif
+int _putchar(char c);
+int main(int argc, char* argv[]);
+
+#endif /*MAIN_H*/
